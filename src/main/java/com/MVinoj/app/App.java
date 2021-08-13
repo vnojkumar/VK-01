19c78e2adb5e25a7e8df1c65947a84c5a024d229
vnojkumar <vnojkumar@hotmail.com>
2021-08-13 06:13:19 +0000

    First commit

---
+++ src/main/java/com/MVinoj/app/App.java
@@ -0,0 +1,13 @@
+package com.MVinoj.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
